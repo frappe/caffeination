487abf5f4f64ed4c1297de10d2a9ae51a87400d9
Ankush Menat <ankush@frappe.io>
2024-12-14 11:01:13 +0530

    feat: accept site and bench filter from CLI

--- caffeination/microbenchmarks/run_benchmarks.py
+++ caffeination/microbenchmarks/run_benchmarks.py
@@ -1,32 +1,53 @@
 #!/bin/env python3
 
 import inspect
-import os
 from types import FunctionType
 
 import frappe
 import pyperf
+from frappe.utils import cstr
 
 from caffeination.microbenchmarks import bench_database, bench_orm, bench_redis
 
 BENCHMARK_PREFIX = "bench_"
-BENCHMARK_SITE = os.environ.get("FRAPPE_BENCHMARK_SITE") or "bench.localhost"
 
 
 def run_microbenchmarks():
-	benchmarks = discover_benchmarks()
+	def update_cmd_line(cmd, args):
+		# Pass our added arguments to workers
+		cmd.extend(["--site", args.site])
+		cmd.extend(["--filter", args.benchmark_filter])
 
-	frappe.init(BENCHMARK_SITE)
-	frappe.connect()
+	runner = pyperf.Runner(add_cmdline_args=update_cmd_line)
 
-	runner = pyperf.Runner()
+	runner.argparser.add_argument(
+		"--filter",
+		dest="benchmark_filter",
+		help="Apply a filter to selectively run benchmarks. This is a substring filter.",
+	)
+	runner.argparser.add_argument(
+		"--site", dest="site", help="Frappe site to use for benchmark", required=True
+	)
+
+	args = runner.argparser.parse_args()
+	benchmarks = discover_benchmarks(args.benchmark_filter)
+	setup(args.site)
 	for name, func in benchmarks:
 		runner.bench_func(name, func)
+	teardown(args.site)
 
+
+def setup(site):
+	frappe.init(site)
+	frappe.connect()
+
+
+def teardown(site):
 	frappe.destroy()
 
 
-def discover_benchmarks():
+def discover_benchmarks(benchmark_filter=None):
+	benchmark_filter = cstr(benchmark_filter)
 	benchmark_modules = [
 		bench_orm,
 		bench_database,
@@ -38,8 +59,9 @@ def discover_benchmarks():
 		module_name = module.__name__.split(".")[-1]
 		for fn_name, fn in inspect.getmembers(module, predicate=lambda x: isinstance(x, FunctionType)):
 			if fn_name.startswith(BENCHMARK_PREFIX):
-				unique_name = f"{module_name}_{fn.__name__.removeprefix(BENCHMARK_PREFIX)}"
-				benchmarks.append((unique_name, fn))
+				unique_name = f"{module_name}_{fn.__name__}"
+				if benchmark_filter in unique_name:
+					benchmarks.append((unique_name, fn))
 
 	return sorted(benchmarks, key=lambda x: x[0])
 
