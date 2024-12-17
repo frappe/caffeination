7d3911291d4375639caed5498bd97905cbfad29c
Ankush Menat <ankush@frappe.io>
2024-12-17 12:06:40 +0530

    fix: improve reliability of cache benches by introducing "fake work"

--- caffeine/microbenchmarks/bench_utils.py
+++ caffeine/microbenchmarks/bench_utils.py
@@ -1,16 +1,21 @@
+import time
+
 from frappe.utils.caching import site_cache
 
 
 @site_cache
 def bench_site_cache_no_arg():
+	time.sleep(0.1)
 	return 42
 
 
 @site_cache
 def bench_site_cache_many_args(x=4, y="abc", z=1.22):
+	time.sleep(0.1)
 	return 42
 
 
 @site_cache(ttl=600)
 def bench_site_cache_with_ttl():
+	time.sleep(0.1)
 	return 42
