afafa44c197c6eac81d4498dcf7ae15769f6442b
Ankush Menat <ankush@frappe.io>
2024-12-17 18:22:40 +0530

    bench: @redis_cache

--- caffeine/microbenchmarks/bench_utils.py
+++ caffeine/microbenchmarks/bench_utils.py
@@ -1,6 +1,7 @@
 import time
 
-from frappe.utils.caching import request_cache, site_cache
+import frappe
+from frappe.utils.caching import redis_cache, request_cache, site_cache
 
 
 @site_cache
@@ -25,3 +26,20 @@ def bench_site_cache_with_ttl():
 def bench_request_cache_many_args(x=4, y="abc", z=1.22):
 	time.sleep(0.1)
 	return 42
+
+
+def bench_redis_cache_deco_with_local_cache():
+	for i in range(100):
+		cache_in_redis(i)
+
+
+def bench_redis_cache_deco_without_local_cache():
+	for i in range(100):
+		cache_in_redis(i)
+	frappe.local.cache.clear()
+
+
+@redis_cache
+def cache_in_redis(num):
+	time.sleep(0.001)
+	return num
