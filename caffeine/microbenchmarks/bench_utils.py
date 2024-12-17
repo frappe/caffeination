20c366b4d1d9049e6bb7883468405848f38b7cc0
Ankush Menat <ankush@frappe.io>
2024-12-17 12:52:04 +0530

    bench: @request_cache

--- caffeine/microbenchmarks/bench_utils.py
+++ caffeine/microbenchmarks/bench_utils.py
@@ -1,6 +1,6 @@
 import time
 
-from frappe.utils.caching import site_cache
+from frappe.utils.caching import request_cache, site_cache
 
 
 @site_cache
@@ -17,5 +17,11 @@ def bench_site_cache_many_args(x=4, y="abc", z=1.22):
 
 @site_cache(ttl=600)
 def bench_site_cache_with_ttl():
+	time.sleep(0.1)
+	return 42
+
+
+@request_cache
+def bench_request_cache_many_args(x=4, y="abc", z=1.22):
 	time.sleep(0.1)
 	return 42
