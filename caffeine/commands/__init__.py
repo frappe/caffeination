19e70828e61e1929eefd9e0bd3956e599ef11fb1
Ankush Menat <ankush@frappe.io>
2024-12-19 11:16:50 +0530

    feat: Easier entry point using bench commands
    Use `bench --site sitename run-microbenchmarks` to run all benchmarks.
    
    Use `--filter=substring` to filter benchmarks. There are no other custom
    flags, rest are inherited from `pyperf`.
    
    Use `bench --site sitename run-microbenchmarks --help` or refer to https://pyperf.readthedocs.io/

---
+++ caffeine/commands/__init__.py
@@ -0,0 +1,32 @@
+import subprocess
+
+import click
+from frappe.commands import pass_context
+from frappe.exceptions import SiteNotSpecifiedError
+
+
+@click.command(
+	"run-microbenchmarks",
+	context_settings=dict(
+		ignore_unknown_options=True,
+	),
+	add_help_option=False,
+)
+@click.argument("benchargs", nargs=-1, type=click.UNPROCESSED)
+@pass_context
+def run_benchmarks(ctx, benchargs):
+	if not ctx.sites:
+		raise SiteNotSpecifiedError
+	site = ctx.sites[0]
+	benchargs = ("--site", site) + benchargs
+
+	from caffeine.microbenchmarks import run_benchmarks
+
+	# XXX: We can't invoke it directly pyperf wants to be the entry point
+	# Anyway, this shouldn't be a problem. It's no different than shell invoking it.
+	subprocess.check_call(["../env/bin/python3", run_benchmarks.__file__, *benchargs])
+
+
+commands = [
+	run_benchmarks,
+]
